93a6a2de6e465b7d9863a4370b06a21f43306bc2
Duvan Mina <129365067+DuAndresMina@users.noreply.github.com>
2025-04-02 14:36:44 -0500

    Update base path in astro.config.mjs and add global CSS file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://duandresmina.github.io',
-  base: 'Abogados-landing',
+  base: '/Abogados-landing',
 });
